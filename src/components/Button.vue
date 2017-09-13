ab19861cffac5989c9d7afbd9f6bb568848cc0db
Maximilian Gustafsson <maxrgus@Maximilians-MacBook-Pro.local>
2017-09-13 12:36:28 +0200

    Initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,58 @@
+<template>
+  <button :class="classes" :type="nativeType ":disabled="disabled">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+  export default {
+    props: {
+      type: {
+        type: String,
+        required: true
+      },
+      nativeType: {
+        type: String,
+        default: 'button'
+      },
+      plain: {
+        type: Boolean,
+        default: false
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      }
+    },
+    computed: {
+      classes() {
+        return [
+          `button--${this.type}`,
+          {
+            'is-plain': this.plain,
+            'is-disabled': this.disabled
+          }
+        ];
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  .button {
+    background: red;
+
+    &.is-disabled {
+      opacity: .4;
+      cursor: not-allowed;
+    }
+  }
+  .button--submit {
+    @extend .button;
+  }
+  .button--cancel {
+    @extend .button;
+    color: #fff;
+    background: green;
+  }
+
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <v-button type="submit">
+      Send
+    </v-button>
+    <v-button type="cancel" :disabled="true">
+      Cancel
+    </v-button>
+  </main>
+</template>
+<script>
+  import VButton from '../components/Button'
+  export default {
+    components: {
+      VButton
+    }
+  }
+</script>
+<style></style>
